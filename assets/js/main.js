9d5ef0b0b530a0294890e89686310350089eb15b
VoVyKhang <vo.vykhang24@gmail.com>
2022-06-19 12:42:21 +0700

    Change background header

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,3 +24,16 @@ function linkAction() {
 }
 
 navLink.forEach(navLinkElement => navLinkElement.addEventListener('click', linkAction))
+
+// Change background header
+function scrollHeader(){
+    const header = document.getElementById('header')
+
+    if(this.scrollY >= 80){
+        header.classList.add('scroll-header')
+    }else{
+        header.classList.remove('scroll-header')
+    }
+}
+
+window.addEventListener('scroll', scrollHeader)
