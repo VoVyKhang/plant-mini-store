d0ccca42a4b3fea7ad74112fce7fe4afd48579cd
VoVyKhang <vo.vykhang24@gmail.com>
2022-06-20 14:49:12 +0700

    Scroll up

--- assets/js/main.js
+++ assets/js/main.js
@@ -87,3 +87,16 @@ function scrollActive(){
 }
 window.addEventListener('scroll', scrollActive)
 
+// ===== Show scroll up =====
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up')
+
+    if(this.scrollY >= 200){
+        scrollUp.classList.add('show-scroll')
+    }else{
+        scrollUp.classList.remove('show-scroll')
+
+    }
+}
+
+window.addEventListener('scroll', scrollUp)
